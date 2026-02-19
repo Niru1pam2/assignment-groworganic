69f5daa1c7a5767ded911038f383117d6e276530
Niru1pam2 <nirupamtalukdar123@gmail.com>
2026-02-19 16:59:24 +0530

    Added axios and rendered initial data

--- app/routes/root/index.tsx
+++ app/routes/root/index.tsx
@@ -1,23 +1,50 @@
-import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { useEffect, useState } from "react";
+import api from "~/lib/axios";
+import type { ApiResponse, Artwork } from "~/types";
 
-const products = [
-  {
-    code: "123",
-    name: "Soap",
-    category: "Home",
-    quantity: 1,
-  },
-];
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 export default function ArtWorks() {
+  const [artworks, setArtworks] = useState<Artwork[]>([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const response = await api.get<ApiResponse>("?page=1");
+
+        setArtworks(response.data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    loadData();
+  }, []);
+
   return (
-    <div>
-      <DataTable value={products} tableStyle={{ minWidth: "50rem" }}>
-        <Column field="code" header="Code"></Column>
-        <Column field="name" header="Name"></Column>
-        <Column field="category" header="Category"></Column>
-        <Column field="quantity" header="Quantity"></Column>
+    <div className="w-full p-4">
+      <DataTable
+        value={artworks}
+        dataKey="id"
+        tableStyle={{ minWidth: "60rem" }}
+        stripedRows
+      >
+        <Column field="title" header="Title" align={"left"}></Column>
+        <Column
+          field="place_of_origin"
+          header="Place of Origin"
+          align={"left"}
+        ></Column>
+        <Column field="artist_display" header="Artist" align={"left"}></Column>
+        <Column
+          field="inscriptions"
+          header="Inscriptions"
+          align={"left"}
+        ></Column>
+        <Column field="date_start" header="Start Date" align={"left"}></Column>
+        <Column field="date_end" header="End Date" align={"left"}></Column>
       </DataTable>
     </div>
   );
--- app/routes/root/root-layout.tsx
+++ app/routes/root/root-layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="flex items-center justify-center min-h-screen w-full max-w-6xl mx-auto p-4">
       <Outlet />
     </div>
   );
