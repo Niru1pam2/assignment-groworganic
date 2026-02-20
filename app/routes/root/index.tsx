04cc143e6da2bc63f3c69e6ccdb9f5dc400afa82
Niru1pam2 <nirupamtalukdar123@gmail.com>
2026-02-21 00:27:34 +0530

    Pagination completed

--- app/routes/root/index.tsx
+++ app/routes/root/index.tsx
@@ -4,32 +4,57 @@ import { useEffect, useState } from "react";
 import api from "~/lib/axios";
 import type { ApiResponse, Artwork } from "~/types";
 
+import { Button } from "primereact/button";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import { getPaginationNumbers } from "~/lib/paginationHelper";
 
 export default function ArtWorks() {
   const [artworks, setArtworks] = useState<Artwork[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(1);
+
+  const [totalEntries, setTotalEntries] = useState<number>(1);
 
   useEffect(() => {
     const loadData = async () => {
       try {
-        const response = await api.get<ApiResponse>("?page=1");
-
+        const response = await api.get<ApiResponse>(`?page=${currentPage}`);
+        console.log("RESPONSE", response);
         setArtworks(response.data.data);
+        setTotalPages(response.data.pagination.total_pages);
+        setTotalEntries(response.data.pagination.total);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     loadData();
-  }, []);
+  }, [currentPage]);
+
+  const onNextPageClick = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
+  const onPrevPageClick = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
+
+  const pagesInBetween = getPaginationNumbers(currentPage);
 
   return (
-    <div className="w-full p-4">
+    <div className="w-full p-4 space-y-5">
       <DataTable
         value={artworks}
         dataKey="id"
         tableStyle={{ minWidth: "60rem" }}
         stripedRows
+        size="small"
+        rows={12}
+        first={(currentPage - 1) * 12}
       >
         <Column field="title" header="Title" align={"left"}></Column>
         <Column
@@ -42,10 +67,46 @@ export default function ArtWorks() {
           field="inscriptions"
           header="Inscriptions"
           align={"left"}
+          body={(rowData: Artwork) => rowData.inscriptions || "NA"}
         ></Column>
         <Column field="date_start" header="Start Date" align={"left"}></Column>
         <Column field="date_end" header="End Date" align={"left"}></Column>
       </DataTable>
+
+      <div className="flex items-center justify-between border-t p-4">
+        <div>
+          <span className="text-sm text-gray-500">
+            Showing {currentPage * 12 - 12 + 1} to {currentPage * 12} of{" "}
+            {totalEntries} entries
+          </span>
+        </div>
+        <div className="flex gap-2 items-center justify-center">
+          <Button
+            onClick={onPrevPageClick}
+            disabled={currentPage === 1}
+            size="small"
+          >
+            Prev
+          </Button>
+          {pagesInBetween.map((p) => (
+            <Button
+              severity={currentPage === p ? "secondary" : "info"}
+              key={p}
+              size="small"
+              onClick={() => setCurrentPage(p)}
+            >
+              {p}
+            </Button>
+          ))}
+          <Button
+            onClick={onNextPageClick}
+            disabled={currentPage === totalPages}
+            size="small"
+          >
+            Next
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
